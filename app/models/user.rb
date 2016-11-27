209a46dd8eb779e258f8cba07a4832c328fb7a9f
Cesar Luque <cesarluque2000@gmail.com>
2016-11-27 06:25:34 +0000

    papaerclip annd aws agian

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,13 +8,8 @@ class User < ActiveRecord::Base
   has_many :articles
   has_many :comments 
   
-<<<<<<< HEAD
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" },
-  default_url: "https://s3.amazonaws.com/rorpost/536989_10151266339362308_1763871769_n.jpg"
-=======
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
-  #, default_url: "/images/:style/missing.png"
->>>>>>> paperclip
+  # , default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   
 end
