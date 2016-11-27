4a7d5bbc4c4e9c4708b4d88f3f8573d8aaab5e7b
Cesar Luque <cesarluque2000@gmail.com>
2016-11-27 05:35:37 +0000

    paperclip and aws

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,8 @@ class User < ActiveRecord::Base
   has_many :articles
   has_many :comments 
   
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" },
+  default_url: "https://s3.amazonaws.com/rorpost/536989_10151266339362308_1763871769_n.jpg"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  
 end
